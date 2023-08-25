40bff6b7b0943b1d76dfcbc4d39cd53dc746b745
Ismael Santos <isant028@fiu.edu>
2023-08-24 21:30:03 -0400

    Updated footer divs and styling

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,21 @@
 import "./Footer.scss";
+import footerLogo from '../../assets/Royal_Caribbean_Group_footer_Logo.jpg';
 
 function Footer() {
 
     return (
-        <></>
+        <>
+        <div className="footer">
+            <div className="footer__logos">
+
+                <img src={footerLogo} className="footer__image" alt="Royal Footer Logo"></img>
+
+
+
+            </div>
+
+        </div>
+        </>
     )
 }
 
